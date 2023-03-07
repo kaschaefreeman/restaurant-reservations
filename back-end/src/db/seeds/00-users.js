c6388ecc1a39517984c8befda8dec534fdd0645f
Kaschae Freeman <kaschaefreeman@gmail.com>
2023-03-07 02:13:59 -0500

    format with prettier

--- back-end/src/db/seeds/00-users.js
+++ back-end/src/db/seeds/00-users.js
@@ -1,18 +1,18 @@
 const { genPassword } = require("../../utils/password-utils");
 const users = require("./00-users.json");
 
-const getUsersWithPassword = async ()=>{
+const getUsersWithPassword = async () => {
   const password = await genPassword(process.env.SEED_PASSWORD)
-  return users.map(user=>user = {...user,password})
+  return users.map(user => user = { ...user, password })
 }
 
-exports.seed =(knex)=> {
+exports.seed = (knex) => {
   return (
-      // Deletes ALL existing entries
-      knex.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+    // Deletes ALL existing entries
+    knex.raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
       // Generate array of users with hashed password
       .then(getUsersWithPassword)
       // Insert seed entries
-      .then(data=>knex("users").insert(data))
+      .then(data => knex("users").insert(data))
   )
 };
